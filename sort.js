f7de7a8c2fa8ea8a2161b2264a848baaacd99c47
y-shadow <yuji.ishado@gmail.com>
2018-07-18 18:50:59 -0700

    Fix some problems.

--- sort.js
+++ sort.js
@@ -1,6 +1,6 @@
 var sortNumber = function (number) {
   number.sort(function (a, b) {
-    if (a == b) {
+    if (a === b) {
       return 0;
     }
     return a < b ? -1 : 1;
@@ -8,5 +8,5 @@ var sortNumber = function (number) {
 };
 
 var number = [19, 3, 81, 1, 24, 21];
-console.log(number);
+sortNumber(number);
 console.log(number);
